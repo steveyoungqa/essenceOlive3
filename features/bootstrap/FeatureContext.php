e1960d4b6b76ac36130ad2f3ba6d064908ba7c8d
steve.young <nocturnalsteve@gmail.com>
2015-04-09 09:49:07 +0100

    Added improved Screenshot on erorr functionality - to fix Jenkins unable to find session error?

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,6 +4,11 @@ use Symfony\Component\Finder\Finder;
 use \SensioLabs\Behat\PageObjectExtension\Context\PageObjectContext;
 use Behat\Behat\Event\StepEvent;
 
+define('SCREENSHOT_PATH', 'screenshots');
+define('SCREENSHOT_URL', 'http://screenshots');
+define('HTML_DUMP_PATH', 'screenshots');
+define('HTML_DUMP_URL', 'http:/screenshots');
+
 
 //class FeatureContext extends PageObjectContext implements  \Behat\MinkExtension\Context\MinkAwareInterface
 class FeatureContext extends Behat\MinkExtension\Context\MinkContext
@@ -41,24 +46,61 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext
     }
 
     /**
-     * Take screenshot when step fails.
-     * Works only with Selenium2Driver.
-     *
      * @AfterStep
      */
-    public function takeScreenshotAfterFailedStep($event)
+    public function dumpInfoAfterFailedStep(StepEvent $event)
     {
-        if (4 === $event->getResult()) {
-            $driver = $this->getSession()->getDriver();
-//            if (!($driver instanceof Selenium2Driver)) {
-////                throw new UnsupportedDriverActionException('Taking screenshots is not supported by %s, use Selenium2Driver instead.', $driver);
-//            }
-            $directory = 'error screenshots/' . $event->getLogicalParent()->getFeature()->getTitle();
-            if (!is_dir($directory)) {
-                mkdir($directory, 0777, true);
+
+        if ($event->getResult() === StepEvent::FAILED)
+        {
+            $session = $this->getSession();
+            $page = $session->getPage();
+            $driver = $session->getDriver();
+            $message = '';
+
+            $fileName = date('D-j-M-H-i-s-A');
+
+            if (defined('HTML_DUMP_PATH'))
+            {
+                if (!file_exists(HTML_DUMP_PATH))
+                {
+                    mkdir(HTML_DUMP_PATH);
+                }
+
+                $date = date('Y-m-d H:i:');
+                $url = $session->getCurrentUrl();
+                $html = $page->getContent();
+
+                $html = "<!-- HTML dump from behat  \nDate: $date  \nUrl:  $url  -->\n " . $html;
+
+                $htmlCapturePath = HTML_DUMP_PATH . '/' . $fileName . '.html';
+                file_put_contents($htmlCapturePath, $html);
+
+//                $message .= "\nHTML saved to: " . HTML_DUMP_PATH . "/". $fileName . ".html";
+//                $message .= "\nHTML available at: " . HTML_DUMP_URL . "/". $fileName . ".html";
             }
-            $filename = sprintf('%s_%s_%s_%s.%s', $event->getLogicalParent()->getTitle(), $this->browserName, date('m-d H-i'), uniqid(''), 'png');
-            file_put_contents($directory . '/' . $filename, $driver->getScreenshot());
+
+            if ($driver instanceof \Behat\Mink\Driver\Selenium2Driver && defined('SCREENSHOT_PATH'))
+            {
+                if (!file_exists(SCREENSHOT_PATH))
+                {
+                    mkdir(SCREENSHOT_PATH);
+                }
+
+                $screenshot = $driver->getScreenshot();
+                $screenshotFilePath = SCREENSHOT_PATH . '/' . $fileName . '.png';
+                file_put_contents($screenshotFilePath, $screenshot);
+
+                $message .= "\nScreenshot saved to: " . SCREENSHOT_PATH . "/". $fileName . ".png";
+//                $message .= "\nScreenshot available at: " . SCREENSHOT_URL . "/". $fileName . ".png";
+            }
+
+            $exception = $event->getException();
+
+            $reflectionObject = new ReflectionObject($exception);
+            $reflectionObjectProp = $reflectionObject->getProperty('message');
+            $reflectionObjectProp->setAccessible(true);
+            $reflectionObjectProp->setValue($exception, $exception->getMessage() . $message);
         }
     }
 
